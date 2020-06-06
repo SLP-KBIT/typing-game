0ffb08ae2df44ab2ac7001d205ebc044b337c0e9
amakuchi <s18t318@stu.kagawa-u.ac.jp>
2020-06-06 18:53:32 +0900

    prettier src/**/.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './assets/App.css';
-import { Scene } from './components/Scene';
+import React from "react";
+import "./assets/App.css";
+import { Scene } from "./components/Scene";
 
 export class App extends React.Component {
   render() {
